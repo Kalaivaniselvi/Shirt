87790e42b9e49346894238aa256b382ebf22803d
unknown <kalaivaniselvi@gmail.com>
2024-08-06 19:08:44 +0530

    Initial changes updates

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -36,30 +36,39 @@ const Home = () => {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={travel} className="d-block w-100" alt="..." style={{ objectFit: "cover", height: "500px" }} />
+            <img
+              src={couple}
+              className="d-block w-100"
+              alt="First slide"
+              style={{ objectFit: "cover", height: "500px" }}
+            />
             <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
+              <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={couple} className="d-block w-100" alt="..." style={{ objectFit: "cover", height: "500px" }} />
+            <img
+              src={travel}
+              className="d-block w-100"
+              alt="Second slide"
+              style={{ objectFit: "cover", height: "500px" }}
+            />
             <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
+              <p>Some representative placeholder content for the second slide.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={order} className="d-block w-100" alt="..." style={{ objectFit: "cover", height: "500px" }} />
+            <img
+              src={order}
+              className="d-block w-100"
+              alt="Third slide"
+              style={{ objectFit: "cover", height: "500px" }}
+            />
             <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
+              <p>Some representative placeholder content for the third slide.</p>
             </div>
           </div>
         </div>
